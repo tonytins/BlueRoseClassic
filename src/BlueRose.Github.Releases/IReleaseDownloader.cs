5404d478622318c07019210e5aa01b68675e44a6
Tony Bark <noreply@tonybark.com>
2021-02-20 20:25:58 -0500

    Fixed formatting in BlueRose.Github.Releases

--- src/BlueRose.Github.Releases/IReleaseDownloader.cs
+++ src/BlueRose.Github.Releases/IReleaseDownloader.cs
@@ -1,11 +1,11 @@
-﻿// This project is licensed under the MIT license.
+// This project is licensed under the MIT license.
 // See the LICENSE file in the project root for more information.
 namespace BlueRose.Github.Releases
 {
- public interface IReleaseDownloader
- {
-  bool IsLatestRelease(string version);
-  bool DownloadLatestRelease();
-  void DeInit();
- }
+    public interface IReleaseDownloader
+    {
+        bool IsLatestRelease(string version);
+        bool DownloadLatestRelease();
+        void DeInit();
+    }
 }
--- src/BlueRose.Github.Releases/IReleaseDownloaderSettings.cs
+++ src/BlueRose.Github.Releases/IReleaseDownloaderSettings.cs
@@ -1,16 +1,16 @@
-﻿// This project is licensed under the MIT license.
+// This project is licensed under the MIT license.
 // See the LICENSE file in the project root for more information.
 using System.Net.Http;
 
 namespace BlueRose.Github.Releases
 {
- public interface IReleaseDownloaderSettings
- {
-  HttpClient HTTPClient { get; set; }
-  string Author { get; set; }
-  string Repository { get; set; }
-  bool IncludePreRelease { get; set; }
-  string DownloadDirPath { get; set; }
-  IReleaseDownloaderSettings Copy();
- }
+    public interface IReleaseDownloaderSettings
+    {
+        HttpClient HTTPClient { get; set; }
+        string Author { get; set; }
+        string Repository { get; set; }
+        bool IncludePreRelease { get; set; }
+        string DownloadDirPath { get; set; }
+        IReleaseDownloaderSettings Copy();
+    }
 }
